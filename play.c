578abdf9595851a2e53b5d8a015c136e33aaa4b0
carwima <36812643+carwima@users.noreply.github.com>
2019-05-11 22:50:46 +0700

    Update play.c

--- play.c
+++ play.c
@@ -11,9 +11,10 @@
 #include <sys/wait.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <dirent.h>
 
 char *queue[];
-int k;
+int k, pstats;
 
 //thread 1
 void* kbhit(void *arg){
@@ -50,18 +51,20 @@ void* kbhit(void *arg){
 void *menu(void *arg){
 	while(1){
 		printf("Now Playing : %s", queue[k]);
-		printf("Previous : Press 4\n", queue[k-1]);
-		printf("Next : Press 2\n", queue[k+1]);
-		printf("Pause : Press 3\n");
+		printf("1. Previous : Press 4\n", queue[k-1]);
+		printf("2. Next : Press 2\n", queue[k+1]);
+		printf("3. Pause : Press 3\n");
 		switch(input){
 		case 1:
-			
+			pstats=1;
+			k--;
 		break;
 		case 2:
-			
+			pstats=1;
+			k++;
 		break;
 		case 3:
-			
+			system("PAUSE");
 		break;
 		case 4:
 			exit(0);
@@ -72,75 +75,80 @@ void *menu(void *arg){
 
 //thread 3
 void *play(void *arg){
-  while(1){
-    if(pflag!=1) continue;
-    else{
-      mpg123_handle *mh;
-      unsigned char *buffer;
-      size_t buffer_size;
-      size_t done;
-      int err;
+	while(1){
+		mpg123_handle *mh;
+		unsigned char *buffer;
+		size_t buffer_size;
+		size_t done;
+		int err;
 
-      int driver;
-      ao_device *dev;
+		int driver;
+		ao_device *dev;
 
-      ao_sample_format format;
-      int channels, encoding;
-      long rate;
+		ao_sample_format format;
+		int channels, encoding;
+		long rate;
+		
+		/* initializations */
+		ao_initialize();
+		driver = ao_default_driver_id();
+		mpg123_init();
+		mh = mpg123_new(NULL, &err);
+		buffer_size = mpg123_outblock(mh);
+		buffer = (unsigned char*) malloc(buffer_size * sizeof(unsigned char));
 
-      if(argc < 2)
-          exit(0);
+		/* open the file and get the decoding format */
+		mpg123_open(mh, queue[k]);
+		mpg123_getformat(mh, &rate, &channels, &encoding);
 
-      /* initializations */
-      ao_initialize();
-      driver = ao_default_driver_id();
-      mpg123_init();
-      mh = mpg123_new(NULL, &err);
-      buffer_size = mpg123_outblock(mh);
-      buffer = (unsigned char*) malloc(buffer_size * sizeof(unsigned char));
+		/* set the output format and open the output device */
+		format.bits = mpg123_encsize(encoding) * BITS;
+		format.rate = rate;
+		format.channels = channels;
+		format.byte_format = AO_FMT_NATIVE;
+		format.matrix = 0;
+		dev = ao_open_live(driver, &format, NULL);
 
-      /* open the file and get the decoding format */
-      mpg123_open(mh, queue[k]);
-      mpg123_getformat(mh, &rate, &channels, &encoding);
-
-      /* set the output format and open the output device */
-      format.bits = mpg123_encsize(encoding) * BITS;
-      format.rate = rate;
-      format.channels = channels;
-      format.byte_format = AO_FMT_NATIVE;
-      format.matrix = 0;
-      dev = ao_open_live(driver, &format, NULL);
-
-      /* decode and play */
-      while (mpg123_read(mh, buffer, buffer_size, &done) == MPG123_OK)
-          ao_play(dev, buffer, done);
-
-      /* clean up */
-      free(buffer);
-      ao_close(dev);
-      mpg123_close(mh);
-      mpg123_delete(mh);
-      mpg123_exit();
-      ao_shutdown();
-    }
-  }
+		/* decode and play */
+		while (mpg123_read(mh, buffer, buffer_size, &done) == MPG123_OK && pstats==0)
+		  ao_play(dev, buffer, done);
+		pstats=0;
+		/* clean up */
+		free(buffer);
+		ao_close(dev);
+		mpg123_close(mh);
+		mpg123_delete(mh);
+		mpg123_exit();
+		ao_shutdown();
+	}
 }
 
-//thread 4
-void *pause(){
-  while(1){
-    
-  }
+//init
+void dir_to_queue(){
+	k=0;
+	DIR *dir;
+	struct dirent *ent;
+	dir = opendir("/home/carwima/FP/mp3");
+	if (dir != NULL) {
+	      while((ent = readdir(dir)) != NULL) {
+		if(ent->d_type==DT_REG){
+			char path[1000];
+			strcpy(path, "/home/carwima/FP/mp3/");
+		        strcat(path, ent->d_name);
+			strcpy(queue[0],path);
+			k++;
+		}
+	      }
+	      closedir(dir);
+	}
 }
 
-//thread 5
-void *dir_to_queue(){
-  
-}
-
-int main(int argc, char *argv[])
-{
-    dir_to_queue(){
-    }
+int main(){
+	dir_to_queue();
+	pthread_create( &th[0], NULL, kbhit, NULL);
+	pthread_create( &th[1], NULL, menu, NULL);
+	pthread_create( &th[2], NULL, play, NULL);
+	int i;
+	for(i=0; i<=3;i++) pthread_join(th[0],NULL);
     return 0;
 }
